8fa0807814314f96efb82a4d688dbf35d6399d1e
yujnishi <yuji_nisimura@yahoo.co.jp>
2018-12-17 00:07:49 +0900

    Update main.sh

--- tests/main.sh
+++ tests/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -vx
+#set -vx
 
 cat << EOF
 #include <CUnit/CUnit.h>
@@ -26,11 +26,22 @@ echo "    CU_initialize_registry();"
 for s in $suites
 do
   echo "${s}_suite = CU_add_suite(\"${s}\",NULL,NULL);"
-  for f in `ctags -x —c-kinds=f ${s}.c`
+  for f in `ctags -x —c-kinds=f ${s}.c | egrep ^test_ | sed 's/\(.*$//'`
   do
     echo "    CU_add_test(${s}_suite,\"${f}\",${f});"
   done
 done
 
+
+cat << EOF
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    printf("\n");
+    CU_basic_show_failures(CU_get_failure_list());
+    printf("\n\n");
+    CU_automated_run_tests();
+    CU_cleanup_registry();
     
-    
+    return 0;
+}
+EOF
