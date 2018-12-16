9507c89084d12c6b5a9c2dd980c41f0d205e825c
yujnishi <yuji_nisimura@yahoo.co.jp>
2018-12-17 01:12:31 +0900

    Update assert.h

--- include/assert.h
+++ include/assert.h
@@ -1,6 +1,6 @@
 #ifndef ASSERT_H
 #define ASSERT_H 1
 
-#define assert(expr) if(expr){fprintf(stderr,"%s(%d): assert: "##expr,__FILE__,__LINE__);abort();}
+#define assert(expr) if(expr){fprintf(stderr,"%s(%d): assert: "#expr,__FILE__,__LINE__);abort();}
 
 #endif /* ASSERT_H */
