9b0f877afa7596aff50de4e758e01d5d92f48326
yujnishi <yuji_nisimura@yahoo.co.jp>
2018-12-16 23:59:03 +0900

    Update main.sh

--- tests/main.sh
+++ tests/main.sh
@@ -16,4 +16,21 @@ do
   suites=$suites\ ${t%.c}
 done
 
-echo $suites
+for s in $suites
+do
+  echo “    CU_pSuite ${s}_suite;”
+done
+
+echo “    CU_initialize_registry();”
+
+for s in $suites
+do
+  echo “${s}_suite = CU_add_suite(\"${s}\",NULL,NULL);”
+  for f in `ctags -x —c-kinds=f`
+  do
+    echo “    CU_add_test(${s}_suite,\"${f}\",${f});”
+  done
+done
+
+    
+    
