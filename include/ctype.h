c3cc2801ca3763aa764f732767687b3f5dd7b1b8
yujnishi <yuji_nisimura@yahoo.co.jp>
2018-12-27 09:33:23 +0900

    Update ctype.h

--- include/ctype.h
+++ include/ctype.h
@@ -1,7 +1,6 @@
-#ifndef 
+#ifndef CTYPE_H
+#define CTYPE_H 1
 
-#de
-iscntrl - 制御文字かどうかの判別
 isdigit - 数字かどうかの判別
 isgraph - 空白（' '）を除く表示文字かどうかの判別
 islower - 小文字かどうかの判別
@@ -20,4 +19,6 @@ tolower
 #endif
 #ifndef istermcntl
 #define istermcntl(c) (0)
+#endif
 #define iscntl(c) (((c)<' '&&!isspace(c))||isoscntl(c)||istermcntl(c))
+#define isdigit(c) ((c)>='
