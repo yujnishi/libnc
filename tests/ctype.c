324db2221adbfa1ebbac08711a92a9720948ecb5
yujnishi <yuji_nisimura@yahoo.co.jp>
2018-12-27 19:57:22 +0900

    Update ctype.c

--- tests/ctype.c
+++ tests/ctype.c
@@ -4,5 +4,10 @@ void test_isalnum() {
     CU_ASSERT(isalnum('A'));
     CU_ASSERT(isalnum('a'));
     CU_ASSERT(isalnum('0'));
-    CU_ASSERT(isalnum('!'));
+    CU_ASSERT(!isalnum('!'));
 }
+
+void test_isalpha() {
+    CU_ASSERT(isalpha('A'));
+    CU_ASSERT(isalpha('a'));
+    CU_ASSERT(
