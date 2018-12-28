3cebccbb6d84dbf6cb52d5b6770724b242ef8bd1
yujnishi <yuji_nisimura@yahoo.co.jp>
2018-12-28 18:27:09 +0900

    Update ctype.c

--- tests/ctype.c
+++ tests/ctype.c
@@ -10,4 +10,62 @@ void test_isalnum() {
 void test_isalpha() {
     CU_ASSERT(isalpha('A'));
     CU_ASSERT(isalpha('a'));
-    CU_ASSERT(
+    CU_ASSERT(!isalpha('0'));
+}
+
+void test_iscntl() {
+    CU_ASSERT(iscntl('\b'));
+    CU_ASSERT(!iscntl('a'));
+}
+
+void test_isdigit() {
+    CU_ASSERT(isdigit('0'));
+    CU_ASSERT(isdigit('9'));
+    CU_ASSERT(!isdigit('a'));
+}
+
+void test_isxdigit() {
+    CU_ASSERT(isxdigit('0'));
+    CU_ASSERT(isxdigit('9'));
+    CU_ASSERT(isxdigit('f'));
+    CU_ASSERT(isxdigit('F'));
+    CU_ASSERT(!isxdigit('g'));
+}
+
+void test_isgraph() {
+    CU_ASSERT(isgraph('a'));
+    CU_ASSERT(isgraph('"'));
+    CU_ASSERT(isgraph('@'));
+    CU_ASSERT(!isgraph('\t'));
+}
+
+void test_islower() {
+    CU_ASSERT(islower('a'));
+    CU_ASSERT(islower('z'));
+    CU_ASSERT(!islower('A'));
+}
+
+void test_isupper() {
+    CU_ASSERT(isupper('A'));
+    CU_ASSERT(isupper('Z'));
+    CU_ASSERT(!isupper('a'));
+}
+
+void test_isprint() {
+    CU_ASSERT(isprint('a'));
+    CU_ASSERT(isprint('"'));
+    CU_ASSERT(isprint('@'));
+    CU_ASSERT(!isprint('\t'));
+}
+
+void test_punct() {
+    CU_ASSERT(ispunct('!'));
+    CU_ASSERT(ispunct('.'));
+    CU_ASSERT(!ispunct('a'));
+}
+
+void test_isspace() {
+    CU_ASSERT(isspace(' '));
+    CU_ASSERT(isspace('\t'));
+    CU_ASSERT(!isspace('\b'));
+}
