9ee307ee8eb1f7829ea083188c0b94f2e67a5468
yujnishi <yuji_nisimura@yahoo.co.jp>
2019-01-08 22:55:17 +0900

    Update ctype.c

--- tests/ctype.c
+++ tests/ctype.c
@@ -78,7 +78,6 @@ void test_nextc() {
     CU_ASSERT(strcmp(p=nextc(p),"αあ𠮷") == 0);
     CU_ASSERT(strcmp(p=nextc(p),"あ𠮷") == 0);
     CU_ASSERT(strcmp(p=nextc(p),"𠮷") == 0);
-    printf("%p:%s\n",p,p);
-    CU_ASSERT( *p == '\0' );
+    CU_ASSERT( *(p=nextc(p)) == '\0' );
     CU_ASSERT( nextc(p) == NULL );
 }
