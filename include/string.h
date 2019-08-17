89a81620a09d408fbf4f5d578dd924dce213133b
Yuji Nishimura <yuji_nisimura@yahoo.co.jp>
2019-08-17 15:14:06 +0900

    mod

---
+++ include/string.h
@@ -0,0 +1,11 @@
+#ifndef STRING_H
+#define STRING_H 1
+
+#include </usr/include/string.h>
+int strpul(char*,const char*,const char*);
+char* strdef(char*,char*);
+char* strdel(char*,const char*);
+int strcvl(char*,int,int);
+
+
+#endif /* STRING_H */
